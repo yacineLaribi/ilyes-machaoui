1da9ae494ae431936b05c0c8a5ca97df0046249e
YacineLaribi <lyacine230@gmail.com>
2026-02-17 19:23:56 +0100

    added meta descriptions and SEO deidcated url

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ app_name='core'
 
 urlpatterns = [
     path("",views.index, name="home"),
+    path("machaoui-ilyes-oran/", views.index, name="machaoui_seo"),
+
     path("latest-order-meta/", views.latest_order_meta, name="latest_order_meta"),
     path("submit-form/",views.submit_feedback,name="submit_form"),
 
